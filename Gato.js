6f5efe5f137f4e37e08de4178ec2b3bd46682dcb
marcosanthony7 <marcosanthony7>
2024-03-18 09:17:20 -0300

    quebra de linhas nome e idade

--- Gato.js
+++ Gato.js
@@ -3,7 +3,8 @@ import { View, Text } from "react-native";
 const Gato = ({nome, idade}) => {
     return(
         <View>
-            <Text>Meu nome é {nome} e tenho {idade} anos.</Text>
+            <Text>Nome: {nome}</Text>
+            <Text>Idade: {idade} anos</Text>
         </View>
     );
 }
