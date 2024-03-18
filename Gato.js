11a75fa47c9f32b3e2dd9ebfcea700526d23a6d2
marcosanthony7 <marcosanthony7>
2024-03-18 08:20:59 -0300

    initial commit

---
+++ Gato.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Gato = () => {
+    return(
+        <View>
+            <Text>Meu nome é Marcos.</Text>
+        </View>
+    );
+}
+
+export default Gato;
