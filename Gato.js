cb1ec85ef3875fd9876ab4a7fef3a623b6ae0d94
marcosanthony7 <marcosanthony7>
2024-03-18 09:26:44 -0300

    implementação texto anos ou ano na idade

--- Gato.js
+++ Gato.js
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 
 const Gato = ({nome, idade}) => {
+    const texto = idade > 1 ? "anos" : "ano";
+
     return(
         <View>
             <Text>Nome: {nome}</Text>
-            <Text>Idade: {idade} anos</Text>
+            <Text>Idade: {idade} {texto}</Text>
         </View>
     );
 }
